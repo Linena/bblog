3a049ba5fa08d70afa1f82f9c589c0fab9be65b5
Linena <2776217800@qq.com>
2022-01-23 15:34:58 +0800

    Sun Jan 23 15:34:57     2022

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,6 @@ set -e
 # 备份项目到仓库中
 git add .
 git commit -m "`date` "
-git config --global user.name "Linena"
-git config --global user.email "2776217800@qq.com"
-git remote set-url origin git@github.com:Linena/linena.github.io.git
 git push origin master
 
 blue "========================================="
@@ -37,10 +34,7 @@ git commit -m "`date` "
 
 #git push -f https://github.com/Linena/linena.github.io.git master
 
-git config --global user.name "Linena"
-git config --global user.email "2776217800@qq.com"
-git remote add origin git@github.com:Linena/linena.github.io.git
-git push -f origin master
+git push -f git@github.com:Linena/blog.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
