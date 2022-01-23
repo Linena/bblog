e73b9827560e6e6018588d5c7a807f8507c45267
Linena <2776217800@qq.com>
2022-01-23 15:30:45 +0800

    Sun Jan 23 15:30:45     2022

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ git add .
 git commit -m "`date` "
 git config --global user.name "Linena"
 git config --global user.email "2776217800@qq.com"
-git remote add origin git@github.com:Linena/linena.github.io.git
+git remote set-url origin git@github.com:Linena/linena.github.io.git
 git push origin master
 
 blue "========================================="
